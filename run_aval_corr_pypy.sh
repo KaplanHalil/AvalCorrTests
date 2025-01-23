7cc27f86e6bf6f5583afb0a01b8df2166f3092d7
HalilKaplan <4.halilkaplan@gmail.com>
2025-01-23 15:21:56 +0300

    .

--- run_aval_corr_pypy.sh
+++ run_aval_corr_pypy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Before running change the environment to PyPy with "source myenv/bin/activate"
 
 # Change the algorithm name to the desired algorithm
 ALG=AES_256
@@ -8,22 +9,24 @@ echo "from $ALG import *" > Alg.py
 
 echo Algorithm: $ALG
 
-# Correlation Tests
-
-echo "Running Correlation Tests"
-pypy3 corr_rk_rk.py &
-pypy3 corr_rc_rc.py &
-pypy3 corr_mk_rk.py &
-pypy3 corr_p_rc.py &
-pypy3 corr_mk_rc.py &
-
+# Remove the previous results
+rm -f Results/*
 
 # Avalance Tests
 
 echo "Running Avalanche Tests"
 pypy3 aval_mk_rc.py & 
 pypy3 aval_mk_rk.py & 
-pypy3 aval_p_rc.py 
+pypy3 aval_p_rc.py &
+
+# Correlation Tests
+
+echo "Running Correlation Tests"
+pypy3 corr_rk_rk.py &
+pypy3 corr_mk_rk.py &
+pypy3 corr_p_rc.py &
+pypy3 corr_mk_rc.py &
+pypy3 corr_rc_rc.py 
 
 # Move the drawings to the Results folder
 mv -t Results/ aval_mk-rc.png aval_mk-rk.png aval_p-rc.png corr_mk-rk.png corr_p-rc.png corr_mk-rc.png corr_rk-rk.png corr_rc-rc.png
